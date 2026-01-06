b97fd12174423535fa17d4935c936a6c406e4fa9
shiyas_dx <shiyasdev62@gmail.com>
2026-01-06 12:09:54 +0530

    Time: 7 ms (97.06%), Space: 25.8 MB (71.67%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        return len(nums) != len(set(nums))
+        
+            
+            
+        
+            
+            
+        
+        
