477d10904736e9017524160eda5027d411bfc377
shiyas_dx <shiyasdev62@gmail.com>
2026-01-12 15:21:11 +0530

    Time: 4 ms (33.04%), Space: 12.5 MB (12.17%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def buyChoco(self, prices, money):
+        prices.sort()
+
+        cost = prices[0] + prices[1]
+
+        if cost <= money:
+            return money - cost
+        return money
+
+        
